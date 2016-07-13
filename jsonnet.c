bb5eccee344468da31df966cc6371831e078f875
neeke <neeke@php.net>
2016-07-13 23:41:25 +0800

    use libjsonnet 0.8.9

--- jsonnet.c
+++ jsonnet.c
@@ -131,7 +131,7 @@ PHP_MINFO_FUNCTION(jsonnet)
 {
     php_info_print_table_start();
     php_info_print_table_header(2, "JsonNet support", "Enabled");
-    php_info_print_table_row(2, "jsonnet Information", JSONNET_VERSION);
+    php_info_print_table_row(2, "lib jsonnet Information", LIB_JSONNET_VERSION);
     php_info_print_table_row(2, "Version", JSONNET_PHP_VERSION);
     php_info_print_table_row(2, "Author", JSONNET_PHP_AUTHOR);
     php_info_print_table_row(2,"Supports", "https://github.com/Neeke/JsonNet");
